b7761ed93cd7d51c22e369d2f66f9a847088b759
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-02-05 18:40:34 +0100

    firebase

---
+++ ChatApp/src/js/vistes/llistatConfig.js
@@ -0,0 +1,251 @@
+import { MessagesList } from "../classes/MessagesList.js";
+import { GroupsList } from "../../../_commons/js/classes/GroupsList";
+import { Groups } from "../../../_commons/js/classes/Groups";
+import { Messages } from "../classes/Messages.js";
+
+export const formulario01 = () => {
+
+    // Funcio per crear la taula grups
+    function carregaGrups() {
+
+        // Carregar amb localstorage
+/*         var arr1 = new Array();
+
+        var Parent = document.getElementById('taulaGrups');
+
+        if(localStorage.getItem("groups") == null){
+            
+            localStorage.setItem("","groups");
+            var r = Parent.insertRow();
+            var cell1 = r.insertCell();
+            var cell2 = r.insertCell();
+
+            cell1.innerHTML = "Res a mostrar";
+            cell2.innerHTML = "Res a mostrar";
+
+        }else{
+            while(Parent.hasChildNodes())
+            {
+                Parent.removeChild(Parent.firstChild);
+            }
+            arr1 = JSON.parse(localStorage.getItem("groups"));
+            for(let i = 0; i < arr1.length; i++){
+                
+                var r = Parent.insertRow();
+                var cell1 = r.insertCell();
+                var cell2 = r.insertCell();
+
+                cell1.innerHTML = "ID: ".bold() + arr1[i].id;
+                cell2.innerHTML = "NOM: ".bold()+arr1[i].name;
+            }
+        } */
+
+        // Carregar amb firebase
+        var llista = new GroupsList();
+        var Parent = document.getElementById('taulaGrups');
+
+        while(Parent.hasChildNodes())
+        {
+            Parent.removeChild(Parent.firstChild);
+        }
+        const data = llista.obtenirDades();
+        data.then(
+            function(value) {
+                const newArr = value.filter((a) => a);
+                for (let i = 0; i < newArr.length; i++) {
+                    var r = Parent.insertRow();
+                    var cell1 = r.insertCell();
+                    var cell2 = r.insertCell();
+                    cell1.innerHTML = "ID: ".bold() + newArr[i].id;
+                    cell2.innerHTML = "NOM: ".bold()+ newArr[i].name;
+                }
+            }
+        );
+    }
+
+    // Funcio per crear la taula missatges
+    function carregaMissatges() {
+
+        // Carregar amb localstorage
+/*         var arr2 = new Array();
+
+        var Parent = document.getElementById('taulaMissatges');
+
+        if(localStorage.getItem("messages") == null){
+
+            var r = Parent.insertRow();
+            var cell1 = r.insertCell();
+            var cell2 = r.insertCell();
+            var cell3 = r.insertCell();
+
+            cell1.innerHTML = "Res a mostrar";
+            cell2.innerHTML = "Res a mostrar";
+            cell3.innerHTML = "Res a mostrar";
+
+        }else{
+            while(Parent.hasChildNodes())
+            {
+                Parent.removeChild(Parent.firstChild);
+            }
+            arr2 = JSON.parse(localStorage.getItem("messages"));
+            for(let i = 0; i < arr2.length; i++){
+                var r = Parent.insertRow();
+                var cell1 = r.insertCell();
+                var cell2 = r.insertCell();
+                var cell3 = r.insertCell();
+
+                cell1.innerHTML = "ID: ".bold()+arr2[i].id;
+                cell2.innerHTML = "MESSAGE: ".bold()+arr2[i].message;
+                cell3.innerHTML = "DESTINATARI: ".bold()+arr2[i].destinatari;
+            }
+        } */
+
+        // Carregar amb firebase
+        var llista2 = new MessagesList();
+        var Parent = document.getElementById('taulaMissatges');
+
+        while(Parent.hasChildNodes())
+        {
+            Parent.removeChild(Parent.firstChild);
+        }
+        const data = llista2.obtenirDades();
+        
+        data.then(
+            function(value) {
+                const newArr = value.filter((a) => a);
+                for (let i = 0; i < newArr.length; i++) {
+                    var r = Parent.insertRow();
+                    var cell1 = r.insertCell();
+                    var cell2 = r.insertCell();
+                    var cell3 = r.insertCell();
+                    cell1.innerHTML = "ID: ".bold() + newArr[i].id;
+                    cell2.innerHTML = "MISSATGE: ".bold()+ newArr[i].message;
+                    cell3.innerHTML = "DESTINATARI: ".bold()+newArr[i].destinatari;
+                }
+            }
+        );
+    }
+
+    carregaGrups();
+    carregaMissatges();
+
+    
+    // Configuració dels botons
+    document.querySelector("#boto1").addEventListener("click"  ,() => {
+
+        let llista = new GroupsList();
+        var value = document.getElementById("grup").value;
+
+        llista.delGroup(value);
+        carregaGrups();
+
+    });
+
+    document.querySelector("#boto2").addEventListener("click"  ,() => {
+        
+        let llista = new GroupsList();
+        var value = document.getElementById("grup").value;
+
+        llista.findGroup(llista,value);
+    });
+
+    document.querySelector("#boto3").addEventListener("click"  ,() => {
+
+        let llista = new GroupsList();
+        var value = document.getElementById("grup").value;
+        let nou = prompt("Introdueix el nou grup");
+        
+        let objGrp = new Groups(value, nou, value);
+
+        // llista.editGroup(value,nou)
+        
+        llista.setGroup(value, objGrp)
+        carregaGrups();
+
+        
+    });
+
+
+    document.querySelector("#boto4").addEventListener("click"  ,() => {
+
+        let llista = new MessagesList();
+        var value = document.getElementById("msg").value;
+
+        llista.delMessage(value);
+        carregaMissatges();
+    });
+
+    document.querySelector("#boto5").addEventListener("click"  ,() => {
+        
+        let llista = new MessagesList();
+        var value = document.getElementById("msg").value;
+
+        llista.findMessage(llista, value);
+
+    });
+
+    document.querySelector("#boto6").addEventListener("click"  ,() => {
+        
+        let llista = new MessagesList();
+        var idUsuari = document.getElementById("msg").value;
+        let nou = prompt("Introdueix el nou missatge");
+
+        const data = llista.obtenirDades();
+        data.then(
+            function(value) {
+                let id, author, created,pubpriv, destinatari
+
+                const newArr = value.filter((a) => a);
+                for(let i = 0; i < newArr.length; i++){
+                    if(newArr[i].id == idUsuari){
+                        id = newArr[i].id;
+                        author = newArr[i].author_id;
+                        created = newArr[i].created;
+                        pubpriv = newArr[i].pubpriv;
+                        destinatari = newArr[i].destinatari;
+
+                        let objMsg = new Messages(id, author, nou, created, pubpriv, destinatari)
+                        llista.setMessage(idUsuari, objMsg)
+                        carregaMissatges();
+                        break;
+                        
+                    }
+                }
+            }
+        )
+        
+        /* llista.editMessage(idUsuari,nou) */
+        
+    });
+
+
+    document.querySelector("#boto7").addEventListener("click"  ,() => {
+
+        let llista = new MessagesList();
+        var Parent = document.getElementById('taulaFiltratge');
+
+        while(Parent.hasChildNodes())
+        {
+            Parent.removeChild(Parent.firstChild);
+        }
+
+        const data = llista.obtenirDades();
+
+        data.then(
+            function(value) {
+                var valor = document.getElementById("msgf").value;
+                const data2 = llista.filtraMessagesPerText(value, valor);
+
+                for (let i = 0; i < data2.length; i++) {
+                    var r = Parent.insertRow();
+                    var cell1 = r.insertCell();
+                     var cell2 = r.insertCell();
+                     var cell3 = r.insertCell();
+                     cell1.innerHTML = "ID: ".bold() + data2[i].id;
+                     cell2.innerHTML = "MISSATGE: ".bold()+ data2[i].message;
+                     cell3.innerHTML = "DESTINATARI: ".bold()+data2[i].destinatari;
+                 }
+             }
+        );
+    });
+}
---
+++ ChatApp/src/js/vistes/missatgesConfig.js
@@ -0,0 +1,143 @@
+import { Messages } from "../classes/Messages.js";
+import { MessagesList } from "../classes/MessagesList.js";
+import { UsersList } from "../../../_commons/js/classes/UsersList.js";
+import { GroupsList } from "../../../_commons/js/classes/GroupsList.js";
+ 
+export const formulario02 = () => {
+
+    // Inicialitzem llistes
+    let llista = new MessagesList();
+    let llista2 = new UsersList();
+    let llista3 = new GroupsList();
+
+    
+    let dropdown = document.getElementById('destinatari');
+    dropdown.length = 0;
+
+    let dropdown2 = document.getElementById('grup');
+    dropdown2.length = 0;
+
+    let option, option2;
+
+    // Creació del desplegable dels usuaris (url JSON)
+/*     async function usuaris() {
+        const url = await llista2.obtenirDades();
+        const request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        
+        request.onload = function() {
+          if (request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            let option;
+            for (let i = 0; i < data.length; i++) {
+              option = document.createElement('option');
+              option.text = data[i].username;
+              dropdown.add(option);
+            }
+           }
+        }
+        request.send();
+    }
+    usuaris(); */
+
+    // Creació del desplegable dels usuaris (FIREBASE)
+    const data = llista2.obtenirDades();
+
+    data.then(
+        function(value) {
+            for (let i = 0; i < value.length; i++) {
+                option = document.createElement('option');
+                option.text = value[i].username;
+                dropdown.add(option);
+            }
+        }
+    );
+
+/*  // Creació del desplegable dels grups (LOCALSTORAGE)
+
+    let dropdown3 = document.getElementById('grup');
+    
+ 
+    dropdown3.length = 0;
+    var groups = JSON.parse(localStorage.getItem("groups"));
+    for (let i = 0; i < groups.length; i++) {
+        option2 = document.createElement('option');
+        option2.text = groups[i].name;
+        dropdown3.add(option2);
+    } */
+
+
+    // Creació del desplegable dels grups (FIREBASE)
+    const data2 = llista3.obtenirDades();
+    
+    data2.then(
+        function(value) {
+            console.log(value)
+            const newArr = value.filter((a) => a);
+            for (let i = 0; i < newArr.length; i++) {
+                option2 = document.createElement('option');
+                option2.text = newArr[i].name;
+                dropdown2.add(option2);
+            }
+        }
+    );
+    
+
+    // Configuració de la visibilitat (RADIO BUTTONS)
+    document.getElementById('opcio1').checked = true;
+
+    document.querySelector("#opcio1").addEventListener("click"  ,() => {
+    
+        var seleccio = document.getElementById('opcio1');
+        
+        if (seleccio.checked) {
+            document.getElementById('destinatari').disabled = true;
+            document.getElementById('grup').disabled = false;
+        }
+    });
+    
+    document.querySelector("#opcio2").addEventListener("click"  ,() => {
+    
+        var seleccio2 = document.getElementById('opcio2');
+    
+        if (seleccio2.checked) {
+            document.getElementById('destinatari').disabled = false;
+            document.getElementById('grup').disabled = true;
+        }
+    
+    });
+    
+    // Configuració de l'enviament de dades
+    document.querySelector("#botoEnviar").addEventListener("click"  ,() => {
+        
+        event.preventDefault();
+    
+        var seleccio = document.getElementById('opcio1');
+        var message = document.getElementById('missatge').value
+        var seleccionado = document.querySelector('input[name="opcio"]:checked').value;
+        
+        var select = document.getElementById('destinatari');
+        var select2 = document.getElementById('grup');
+        
+    
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    
+        if(seleccio.checked){
+            var grup = select2.options[select2.selectedIndex].value;
+            let objMsg = new Messages(llista.autoincrementId(), 1, message, date+" "+time, seleccionado, grup);
+            llista.setMessage(llista.autoincrementId(), objMsg);
+            console.log(llista.autoincrementId())
+        }else{
+            var usuari = select.options[select.selectedIndex].value;
+            console.log(llista.autoincrementId())
+            let objMsg = new Messages(llista.autoincrementId(), 1, message, date+" "+time, seleccionado, usuari);
+            llista.setMessage(llista.autoincrementId(), objMsg);
+            console.log(llista)
+        }
+
+        alert("Missatge enviat amb exit");
+    
+    });
+}
